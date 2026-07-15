e30cb70ba4154c6a514b4924aaf3e0501c40c558
LCZ <czarnacki.lukasz.rafal@gmail.com>
2026-07-15 11:54:58 +0200

    v0.2.2

--- src/simaticml_decoder/__init__.py
+++ src/simaticml_decoder/__init__.py
@@ -10,4 +10,4 @@ Supporting modules: instructions.py (part catalog, data not logic),
 operand.py (Access -> display string), scl_reconstruct.py (SCL networks).
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
